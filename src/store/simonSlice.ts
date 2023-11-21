8b7c3dd05043d851a8dfc2ed25451903fa7ede81
Ran Matana <ran.matana@msapps.mobi>
2023-11-21 16:56:46 +0200

    add functionality

--- src/store/simonSlice.ts
+++ src/store/simonSlice.ts
@@ -1,7 +1,11 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {RootState} from '.';
 
-interface SimonState {}
+interface SimonState {
+  sequence: string[];
+  userSequence: string[];
+  isPlaying: boolean;
+}
 
 const initialState: SimonState = {
   sequence: [],
@@ -12,10 +16,34 @@ const initialState: SimonState = {
 const simonSlice = createSlice({
   name: 'simon',
   initialState,
-  reducers: {},
+  reducers: {
+    startGame: state => {
+      state.sequence = [];
+      state.userSequence = [];
+      state.isPlaying = true;
+      state.sequence.push(getRandomColor());
+    },
+    pressButton: (state, action: PayloadAction<string>) => {
+      if (state.isPlaying) {
+        state.userSequence.push(action.payload);
+        if (state.userSequence.join('') === state.sequence.join('')) {
+          state.sequence.push(getRandomColor());
+          state.userSequence = [];
+        } else {
+          state.isPlaying = false;
+        }
+      }
+    },
+  },
 });
 
-export const {} = simonSlice.actions;
+const getRandomColor = () => {
+  const colors = ['red', 'green', 'blue', 'yellow'];
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex];
+};
+
+export const {startGame, pressButton} = simonSlice.actions;
 
 export const selectSimon = (state: RootState) => state.simon;
 
