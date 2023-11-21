b70290f8ad1c114c5aee9c35b721682007da2db2
Ran Matana <ran.matana@msapps.mobi>
2023-11-21 17:00:46 +0200

    connect to app

--- App.tsx
+++ App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import {Provider} from 'react-redux';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {SimonGameScreen} from './src/screens';
 import {store} from './src/store';
 
+const Stack = createStackNavigator();
+
 const App: React.FC = () => {
-  return <Provider store={store}></Provider>;
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="SimonGame"
+            component={SimonGameScreen}
+            options={{title: 'Simon Says'}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
 };
 
 export default App;
